0b8118ff9a4f580f86aca301a75adce0ccd75e32
seungminshin1 <163735629+seungminshin1@users.noreply.github.com>
2025-01-24 00:22:39 +0900

    Update Face_recognition_system.py

--- Face_recognition_system.py
+++ Face_recognition_system.py
@@ -1,1 +1,222 @@
+# Updates the package lists for upgrades or new installations
+sudo apt-get update
+sudo apt-get install python3.10 python3.10-distutils python3.10-dev
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 2
+sudo update-alternatives --config python3
 
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10
+
+# Clone the tensorflow models repository from GitHub
+pip uninstall Cython -y 
+git clone --depth 1 https://github.com/tensorflow/models
+
+wget https://github.com/protocolbuffers/protobuf/releases/download/v3.20.3/protoc-3.20.3-linux-x86_64.zip
+unzip protoc-3.20.3-linux-x86_64.zip -d /usr/local/
+export PATH=/usr/local/bin:$PATH
+
+# Copy setup files into models/research folder
+%%bash
+cd models/research/
+protoc object_detection/protos/*.proto --python_out=.
+
+# Modify setup.py file to install the tf-models-official repository targeted at TF v2.8.0
+import re
+with open('/content/models/research/object_detection/packages/tf2/setup.py') as f:
+    s = f.read()
+with open('/content/models/research/setup.py', 'w') as f:
+    # Set fine_tune_checkpoint path
+    s = re.sub('tf-models-official>=2.5.1',
+               'tf-models-official==2.8.0', s)
+    f.write(s)
+
+# Install the Object Detection API
+# Need to do a temporary fix with PyYAML because Colab isn't able to install PyYAML v5.4.1
+pip install pyyaml==5.3
+pip install /content/models/research/
+pip install tensorflow==2.8.0
+
+# Install CUDA version 11.0
+pip install tensorflow_io==0.23.1
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_amd64.deb
+dpkg -i cuda-repo-ubuntu1804-11-0-local_11.0.2-450.51.05-1_amd64.deb
+apt-key add /var/cuda-repo-ubuntu1804-11-0-local/7fa2af80.pub
+apt-get update && sudo apt-get install cuda-toolkit-11-0
+export LD_LIBRARY_PATH=/usr/local/cuda-11.0/lib64:$LD_LIBRARY_PATH
+
+# Run Model Bulider Test file, verify everything's working properly
+python /content/models/research/object_detection/builders/model_builder_tf2_test.py
+
+# Downloads the file images.zip from the provided Dropbox link
+wget -O /content/images.zip https://www.dropbox.com/scl/fi/j6l1l9fypbnboy0mvjosy/images.zip?rlkey=ff72x888mwcv443au7xs77su6&st=3rjeppkh&dl=0
+
+# Split images into train, validation, and test folders
+mkdir /content/images
+unzip -q images.zip -d /content/images/all
+mkdir /content/images/train; mkdir /content/images/validation; mkdir /content/images/test
+
+# Download the script and execute
+wget https://raw.githubusercontent.com/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/master/util_scripts/train_val_test_split.py
+python train_val_test_split.py
+
+# Create Labelmap and TFRecords
+%%bash
+cat <<EOF >> /content/labelmap.txt
+seungmin
+EOF
+
+# Download data conversion scripts
+wget https://raw.githubusercontent.com/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/master/util_scripts/create_csv.py
+wget https://raw.githubusercontent.com/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/master/util_scripts/create_tfrecord.py
+
+# Create CSV data files and TFRecord files
+python3 create_csv.py
+python3 create_tfrecord.py --csv_input=images/train_labels.csv --labelmap=labelmap.txt --image_dir=images/train --output_path=train.tfrecord
+python3 create_tfrecord.py --csv_input=images/validation_labels.csv --labelmap=labelmap.txt --image_dir=images/validation --output_path=val.tfrecord
+
+# Crete TFRecords
+train_record_fname = '/content/train.tfrecord'
+val_record_fname = '/content/val.tfrecord'
+label_map_pbtxt_fname = '/content/labelmap.pbtxt'
+
+# Change the chosen_model variable to deploy different models available in the TF2 object detection zoo
+chosen_model = 'ssd-mobilenet-v2-fpnlite-320'
+MODELS_CONFIG = {
+    'ssd-mobilenet-v2': {
+        'model_name': 'ssd_mobilenet_v2_320x320_coco17_tpu-8',
+        'base_pipeline_file': 'ssd_mobilenet_v2_320x320_coco17_tpu-8.config',
+        'pretrained_checkpoint': 'ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz',
+    },
+    'efficientdet-d0': {
+        'model_name': 'efficientdet_d0_coco17_tpu-32',
+        'base_pipeline_file': 'ssd_efficientdet_d0_512x512_coco17_tpu-8.config',
+        'pretrained_checkpoint': 'efficientdet_d0_coco17_tpu-32.tar.gz',
+    },
+    'ssd-mobilenet-v2-fpnlite-320': {
+        'model_name': 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8',
+        'base_pipeline_file': 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.config',
+        'pretrained_checkpoint': 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz',
+    },
+}
+model_name = MODELS_CONFIG[chosen_model]['model_name']
+pretrained_checkpoint = MODELS_CONFIG[chosen_model]['pretrained_checkpoint']
+base_pipeline_file = MODELS_CONFIG[chosen_model]['base_pipeline_file']
+
+# Create "mymodel" folder for holding pre-trained weights and configuration files
+%mkdir /content/models/mymodel/
+%cd /content/models/mymodel/
+
+# Download pre-trained model weights
+import tarfile
+download_tar = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/' + pretrained_checkpoint
+wget {download_tar}
+tar = tarfile.open(pretrained_checkpoint)
+tar.extractall()
+tar.close()
+
+# Download training configuration file for model
+download_config = 'https://raw.githubusercontent.com/tensorflow/models/master/research/object_detection/configs/tf2/' + base_pipeline_file
+wget {download_config}
+
+# Set training parameters for the model
+num_steps = 10000
+if chosen_model == 'efficientdet-d0':
+  batch_size = 4
+else:
+  batch_size = 16
+
+# Set file locations and get number of classes for config file
+import sys
+sys.path.append('/content/models/research')
+sys.path.append('/content/models/research/slim')
+from object_detection.utils import label_map_util
+pipeline_fname = '/content/models/mymodel/' + base_pipeline_file
+fine_tune_checkpoint = '/content/models/mymodel/' + model_name + '/checkpoint/ckpt-0'
+def get_num_classes(pbtxt_fname):
+    label_map = label_map_util.load_labelmap(pbtxt_fname)
+    categories = label_map_util.convert_label_map_to_categories(
+        label_map, max_num_classes=90, use_display_name=True
+    )
+    category_index = label_map_util.create_category_index(categories)
+    return len(category_index.keys())
+num_classes = get_num_classes(label_map_pbtxt_fname)
+print('Total classes:', num_classes)
+
+# Create custom configuration file by writing the dataset, model checkpoint, and training parameters into the base pipeline file
+import re
+%cd /content/models/mymodel
+print('writing custom configuration file')
+with open(pipeline_fname) as f:
+    s = f.read()
+with open('pipeline_file.config', 'w') as f:
+    s = re.sub('fine_tune_checkpoint: ".*?"',
+               'fine_tune_checkpoint: "{}"'.format(fine_tune_checkpoint), s)
+    s = re.sub(
+        '(input_path: ".*?)(PATH_TO_BE_CONFIGURED/train)(.*?")', 'input_path: "{}"'.format(train_record_fname), s)
+    s = re.sub(
+        '(input_path: ".*?)(PATH_TO_BE_CONFIGURED/val)(.*?")', 'input_path: "{}"'.format(val_record_fname), s)
+    s = re.sub(
+        'label_map_path: ".*?"', 'label_map_path: "{}"'.format(label_map_pbtxt_fname), s)
+    s = re.sub('batch_size: [0-9]+',
+               'batch_size: {}'.format(batch_size), s)
+    s = re.sub('num_steps: [0-9]+',
+               'num_steps: {}'.format(num_steps), s)
+    s = re.sub('num_classes: [0-9]+',
+               'num_classes: {}'.format(num_classes), s)
+    s = re.sub(
+        'fine_tune_checkpoint_type: "classification"', 'fine_tune_checkpoint_type: "{}"'.format('detection'), s)
+    if chosen_model == 'ssd-mobilenet-v2':
+      s = re.sub('learning_rate_base: .8',
+                 'learning_rate_base: .08', s)
+      s = re.sub('warmup_learning_rate: 0.13333',
+                 'warmup_learning_rate: .026666', s)
+    if chosen_model == 'efficientdet-d0':
+      s = re.sub('keep_aspect_ratio_resizer', 'fixed_shape_resizer', s)
+      s = re.sub('pad_to_max_dimension: true', '', s)
+      s = re.sub('min_dimension', 'height', s)
+      s = re.sub('max_dimension', 'width', s)
+    f.write(s)
+
+# Set the path to the custom config file and the directory to store training checkpoints in
+pipeline_file = '/content/models/mymodel/pipeline_file.config'
+model_dir = '/content/training/'
+
+# Load up a TensorBoard session to monitor training progress
+%load_ext tensorboard
+%tensorboard --logdir '/content/training/train'
+
+# Run training
+python /content/models/research/object_detection/model_main_tf2.py \
+    --pipeline_config_path={pipeline_file} \
+    --model_dir={model_dir} \
+    --alsologtostderr \
+    --num_train_steps={num_steps} \
+    --sample_1_of_n_eval_examples=1
+
+# Make a directory to store the trained TFLite model
+mkdir /content/custom_model_lite
+output_directory = '/content/custom_model_lite'
+last_model_path = '/content/training'
+python /content/models/research/object_detection/export_tflite_graph_tf2.py \
+    --trained_checkpoint_dir {last_model_path} \
+    --output_directory {output_directory} \
+    --pipeline_config_path {pipeline_file}
+
+# Convert exported graph file into TFLite model file
+import tensorflow as tf
+converter = tf.lite.TFLiteConverter.from_saved_model('/content/custom_model_lite/saved_model')
+tflite_model = converter.convert()
+with open('/content/custom_model_lite/detect.tflite', 'wb') as f:
+  f.write(tflite_model)
+
+# Move labelmap and pipeline config files into TFLite model folder and zip it up
+cp /content/labelmap.txt /content/custom_model_lite
+cp /content/labelmap.pbtxt /content/custom_model_lite
+cp /content/models/mymodel/pipeline_file.config /content/custom_model_lite
+%cd /content
+zip -r custom_model_lite.zip custom_model_lite
+
+from google.colab import files
+files.download('/content/custom_model_lite.zip')
